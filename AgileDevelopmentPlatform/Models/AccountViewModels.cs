0c781e80a757119b7ad41ebc98717dab8beb6d50
SuceaCosmin <suceacosmin@yahoo.com>
2018-04-15 20:35:01 +0300

    Login functionality has been updated: Username  attribute  has been added; Login operations are performed with username and password.

--- AgileDevelopmentPlatform/Models/AccountViewModels.cs
+++ AgileDevelopmentPlatform/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace AgileDevelopmentPlatform.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,11 @@ namespace AgileDevelopmentPlatform.Models
 
     public class RegisterViewModel
     {
+
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }    
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
